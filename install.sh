92d0316b0e10a3b996b8553e6d32cf42d79d4a6d
Erkan Durmaz <durmaze@gmail.com>
2019-07-21 23:40:13 +0300

    adds conky and nitrogen configs, and adds fonts folder for custom fonts

--- install.sh
+++ install.sh
@@ -3,14 +3,18 @@
 
 brew bundle --file=macos/Brewfile
 
+stow backgrounds
 stow bash
+stow conky
 stow curl
+stow fonts && fc-cache
 stow gcalcli
 stow git
 stow macos
 stow mutt
 stow neofetch
 stow newsboat
+stow nitrogen
 stow ranger
 stow sxhkd
 stow sxiv
